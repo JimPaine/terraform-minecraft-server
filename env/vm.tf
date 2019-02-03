29e5f69b0f33f7d72409f570eec5dab37b3997a2
Jim Paine <jim@jimpaine.io>
2019-02-03 21:47:20 +0000

    Update vm.tf

--- env/vm.tf
+++ env/vm.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_machine" "main" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
